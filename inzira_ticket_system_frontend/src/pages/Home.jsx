9f476c30be71a67a5623d7b98405fafb0774dd49
gabbrenn <gabvladimirbrenn@gmail.com>
2025-08-04 23:10:37 +0200

    Fix Sidebar and home page issue

--- inzira_ticket_system_frontend/src/pages/Home.jsx
+++ inzira_ticket_system_frontend/src/pages/Home.jsx
@@ -216,6 +216,9 @@ const Home = () => {
           </div>
           
       <main className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <BookingReferenceSearch />
+        </main>
+
         </div>
       </div>
     </div>
